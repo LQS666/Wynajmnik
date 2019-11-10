a8da6426c26c2c6a8bef20029f2660a1e549a73d
Mateusz Bierka <mateusz.bierka@gmail.com>
2019-11-10 14:48:39 +0000

    [refactoring]

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/account/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::get('/account/register', 'Auth\RegisterController@showRegistrationForm')->name('account.register');
 Route::post('/account/register', 'Auth\RegisterController@register');
 
-Route::get('/account/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/account/login', 'Auth\LoginController@showLoginForm')->name('account.login');
 Route::post('/account/login', 'Auth\LoginController@login');
 
-Route::post('/account/logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('/account/logout', 'Auth\LoginController@logout')->name('account.logout');
 
-Route::get('/account/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('/account/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('/account/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('/account/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('/account/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('account.password.request');
+Route::get('/account/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('account.password.reset');
+Route::post('/account/password/reset', 'Auth\ResetPasswordController@reset')->name('account.password.update');
+Route::post('/account/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('account.password.email');
 
-Route::get('/account/email/resend', 'Auth\VerificationController@resend')->name('email.resend');
-Route::get('/account/email/verify', 'Auth\VerificationController@show')->name('email.notice');
-Route::get('/account/email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('email.verify');
+Route::get('/account/email/resend', 'Auth\VerificationController@resend')->name('account.email.resend');
+Route::get('/account/email/verify', 'Auth\VerificationController@show')->name('account.email.notice');
+Route::get('/account/email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('account.email.verify');
 
