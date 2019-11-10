eba2ce13a36054d9ff1f6e62110e43cb800cc2d4
Mateusz Bierka <mateusz.bierka@gmail.com>
2019-11-10 14:08:41 +0000

    [feature] Password reset

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
 Route::get('/account/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('/account/register', 'Auth\RegisterController@register');
 
@@ -22,3 +24,8 @@ Route::get('/account/login', 'Auth\LoginController@showLoginForm')->name('login'
 Route::post('/account/login', 'Auth\LoginController@login');
 
 Route::post('/account/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/account/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/account/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/account/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('/account/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
