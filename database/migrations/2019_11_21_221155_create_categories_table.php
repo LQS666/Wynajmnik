84d9256df8b608a6d2084285a7be2213021c1d9f
Mateusz Bierka <mateusz.bierka@gmail.com>
2019-11-21 22:20:00 +0000

    [feature] Categories db tables

---
+++ database/migrations/2019_11_21_221155_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('sub')->default(0);
+            $table->string('slug');
+            $table->string('name');
+            $table->boolean('visible')->default(false);
+            $table->timestamps();
+
+            $table->foreign('sub')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
