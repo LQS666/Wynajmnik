789299502dfb0e18d97fe841675884ef4c541972
Paweł <p.turkot123@gmail.com>
2019-12-29 05:42:58 +0100

    [feature] add loader and auto redirect to payu

--- resources/lang/pl/dashboard/payment.php
+++ resources/lang/pl/dashboard/payment.php
@@ -1,16 +1,15 @@
 <?php
 
 return [
-    'title' => 'Płatności',
+    'title' => 'Płatności - doładowanie konta',
     'id' => 'Nr transakcji',
     'date' => 'Data',
     'points' => 'Ilość punktów',
     'status' => 'Status',
-    'desc' => 'Doładuj konto punktami premium',
-    'loading' => 'W ciągu 5 sekund nastąpi przekierowanie...',
-    'warning' => '*W przypadku problemów z płatnością prosimy spróbować ponownie. <br> Poprzednia płatność zostanie anulowana.',
+    'loading' => 'W ciągu <b>5 sekund</b> nastąpi przekierowanie',
+    'warning' => '*W przypadku problemów z płatnością prosimy spróbować ponownie. <br> Poprzednia płatność zostanie anulowana automatycznie.',
     'history' => 'Historia wykonanych płatności:',
     'return' => 'Powrót',
     'save' => 'Zatwierdź',
-    'empty-table' => 'Brak przedmiotów do wyświetlenia',
+    'empty-table' => 'Brak płatności do wyświetlenia',
 ];
--- resources/views/my-account/payments.blade.php
+++ resources/views/my-account/payments.blade.php
@@ -15,48 +15,48 @@
                 <input type="hidden" name="{{ $name }}" value="{{ $value }}" />
                 @endforeach
             </form>
-            <div class="flex justify-center items-center h-48">{{ __('dashboard/payment.loading') }}</div>
+            <div class="loader">
+                <img class="loader__icon" src="{{ asset('/assets/images/logo_icon.png')}}" />
+                <p class="loading">{!! __('dashboard/payment.loading') !!}</p>
+            </div>
             @else
-            <div class="flex flex-col justify-center items-center my-3">
-                <div class="text-center">
-                    <h4>{{ __('dashboard/payment.desc') }}</h4>
-                    <p>10zł = 1000pkt</p>
-                    <p>20zł = 2000pkt</p>
-                    <p>50zł = 5000pkt</p>
-                    <p>100zł = 10000pkt</p>
-                </div>
-                <div class="flex flex-wrap justify-center items-center my-3">
-                    <form method="POST" action="{{ route('my-account.payments') }}">
+            <div class="payment__options">
+                <div class="payment__items">
+                    <form class="payment__item" method="POST" action="{{ route('my-account.payments') }}">
                         @csrf
                         <input type="hidden" name="amount" value="1000">
-                        <button class="button mx-3" type="submit">10zł</button>
+                        <span>1000pkt</span>
+                        <button class="button mt-1 mx-3" type="submit">10zł</button>
                     </form>
-                    <form method="POST" action="{{ route('my-account.payments') }}">
+                    <form class="payment__item" method="POST" action="{{ route('my-account.payments') }}">
                         @csrf
                         <input type="hidden" name="amount" value="2000">
+                        <span>2000pkt</span>
                         <button class="button mx-3" type="submit">20zł</button>
                     </form>
-                    <form method="POST" action="{{ route('my-account.payments') }}">
+                    <form class="payment__item" method="POST" action="{{ route('my-account.payments') }}">
                         @csrf
                         <input type="hidden" name="amount" value="5000">
+                        <span>5000pkt</span>
                         <button class="button mx-3" type="submit">50zł</button>
                     </form>
-                    <form method="POST" action="{{ route('my-account.payments') }}">
+                    <form class="payment__item" method="POST" action="{{ route('my-account.payments') }}">
                         @csrf
                         <input type="hidden" name="amount" value="10000">
+                        <span>10000pkt</span>
                         <button class="button mx-3" type="submit">100zł</button>
                     </form>
                 </div>
-                <div class="text-sm mb-6">
+                <div class="payment__warning">
                     <p>{!! __('dashboard/payment.warning') !!}</p>
                 </div>
-                <h3 class="mt-12 mb-3 text-xl">{{ __('dashboard/payment.history') }}</h3>
             </div>
             @endif
         </div>
 
         @if (!isset($payu))
         @if (count($payments) > 0)
+        <h3 class="payment__history">{{ __('dashboard/payment.history') }}</h3>
         <div class="table-items">
             <table cellspacing="0">
 
@@ -79,7 +79,8 @@
                         {{ $payment['amount']}}
                     </td>
                     <td class="payment">
-                        {!! $payment['status'] ? '<span class="text-green-600">Opłacaono</span>' : '<span class="text-red-500">Nieopłacono</span>' !!}
+                        {!! $payment['status'] ? '<span class="text-green-600">Opłacaono</span>' : '<span
+                            class="text-red-500">Nieopłacono</span>' !!}
                     </td>
                 </tr>
                 @endforeach
@@ -88,8 +89,8 @@
 
         @else
 
-        <div class="flex justify-center items-center bg-purple-main w-1/2 py-3 rounded-lg">
-            {{ __('dashboard/product.empty-table') }}
+        <div class="payment__empty-list">
+            {{ __('dashboard/payment.empty-table') }}
         </div>
 
         @endif
@@ -99,9 +100,9 @@
 
 @if (isset($payu))
 <script>
-    // setTimeout(function() {
-    //     document.forms['payuform'].submit();
-    // }, 5000);
+    setTimeout(function() {
+        document.forms['payuform'].submit();
+    }, 5000);
 </script>
 @endif
 
