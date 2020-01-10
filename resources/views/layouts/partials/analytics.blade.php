86fca1055aa0a2fcd4284d4098476c14b1cefb62
Paweł <p.turkot123@gmail.com>
2020-01-10 19:50:25 +0100

    [feature] add google analytics

---
+++ resources/views/layouts/partials/analytics.blade.php
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-81TJW1C3N2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-81TJW1C3N2');
+</script>
