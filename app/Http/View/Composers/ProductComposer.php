7a69fb9995aa11b1821c71734ee5948dc8051318
Mateusz Bierka <mateusz.bierka@gmail.com>
2020-01-15 21:34:22 +0000

    [feature] add variables containig products characteristic

--- app/Http/View/Composers/ProductComposer.php
+++ app/Http/View/Composers/ProductComposer.php
@@ -11,11 +11,19 @@ class ProductComposer
 {
     protected $user;
 
+    protected $product;
+
     public function __construct(Request $request) {
         $this->user = $request->user();
     }
 
     public function compose(View $view) {
+        if ($data = $view->getData()) {
+            if (!is_null($data['product'])) {
+                $this->product = $data['product'];
+            }
+        }
+
         $filters = Filter::with('values')->where('visible', true)->get();
 
         $categories = Category::maincategories()->with([
@@ -26,7 +34,31 @@ class ProductComposer
         ->where('visible', true)
         ->get();
 
+        if (!empty($this->product)) {
+            $checked = [
+                'categories' => [],
+                'filters' => [],
+                'filterValues' => [],
+            ];
+
+            if (!empty($this->product->categories)) {
+                foreach ($this->product->categories as $category) {
+                    $checked['categories'][] = $category->id;
+                }
+            }
+
+            if (!empty($this->product->filterValues)) {
+                foreach ($this->product->filterValues as $filterValue) {
+                    $checked['filterValues'][] = $filterValue->id;
+                    if (!in_array($filterValue->filter->id, $checked['filters'])) {
+                        $checked['filters'][] = $filterValue->filter->id;
+                    }
+                }
+            }
+        }
+
         $view->with('filters', $filters);
         $view->with('categories', $categories);
+        $view->with('checked', isset($checked) ? $checked : []);
     }
 }
