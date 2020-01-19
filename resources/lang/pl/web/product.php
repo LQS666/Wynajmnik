b7629d90a6896cad86d0c26e4d2ce26e6d79f083
Paweł <p.turkot123@gmail.com>
2020-01-19 21:23:38 +0100

    [fix] trans

---
+++ resources/lang/pl/web/product.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Przedmiot',
+    'loading' => 'Ładowanie przedmiotu',
+    'goback' => 'Powrót',
+    'desc' => 'Opis',
+    'price' => 'zł',
+    'offer_added' => 'Ogłoszenie dodane',
+    'user_since' => 'Użytkownik od',
+    'message' => 'Napisz wiadomość',
+    'other_items' => 'Pozostałe przedmioty',
+    'reservation' => 'Rezerwacja przedmiotu',
+    'reservation_button' => 'Złóż ofertę rezerwacji',
+    'from' => 'Od',
+    'to' => 'Do',
+    'map' => 'Zobacz jak do nas dojechać',
+];
--- resources/views/web/product.blade.php
+++ resources/views/web/product.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.base')
 
+@section('title', __('web/product.title'))
+
 @section('content')
 
 {{-- <div class="container">
@@ -9,11 +11,11 @@
 
 <main class="product-view">
     <div class="product-view__gallery">
-        <a href="{{ route('web.categories') }}" class="block mt-3">Powrót</a>
+        <a href="{{ route('web.categories') }}" class="block mt-3">{{ __('web/product.goback') }}</a>
         <h3>{{ $product->name }}</h3>
         <div class="loader">
             <img class="loader__icon" src="{{ asset('/assets/images/brand/logo_icon.png')}}" />
-            <p class="loading">Ładowanie przedmiotu</p>
+            <p class="loading">{{ __('web/product.loading') }}</p>
         </div>
         <div class="container">
             <div class="synch-carousels">
@@ -77,16 +79,16 @@
             </div>
         </div>
         <div class="product-view__desc">
-            <h4>Opis</h4>
+            <h4>{{ __('web/product.desc') }}</h4>
             <span>{!! $product->desc !!}</span>
         </div>
     </div>
     <div class="product-view__content">
         <div class="product-view__date">
-            <span>Ogłoszenie dodane {{ \Carbon\Carbon::parse($product->created_at)->format('d.m.Y') }}</span>
+            <span>{{ __('web/product.offer_added') }} {{ \Carbon\Carbon::parse($product->created_at)->format('d.m.Y') }}</span>
         </div>
         <div class="product-view__price">
-            <span>{{ $product->price }} zł</span>
+            <span>{{ $product->price }} {{ __('web/product.price') }}</span>
         </div>
         <div class="product-view__details">
             <div class="flex justify-center">
@@ -99,30 +101,30 @@
                 @endif
             </div>
             <span class="product-view__details__name">{{ $product->owner->name }}</span>
-            <span class="product-view__details__date">Użytkownik od
+            <span class="product-view__details__date">{{ __('web/product.user_since') }}
                 {{ \Carbon\Carbon::parse($product->owner->email_verified_at)->format('d.m.Y') }}</span>
         </div>
         <a href="" class="product-view__contact">
-            <span>Napisz wiadomość</span>
+            <span>{{ __('web/product.message') }}</span>
         </a>
         <a href="{{ route('web.categories') }}" class="product-view__others">
-            <span>Pozostałe przedmioty</span>
+            <span>{{ __('web/product.other_items') }}</span>
         </a>
 
         <form class="product-view__reservation" action="">
-            <h4>Rezerwacja przedmiotu</h4>
+            <h4>{{ __('web/product.reservation') }}</h4>
             <div>	
-                <label>Od</label>		
-                <input type="date" name="dateFrom" value="2019-12-26">	
+                <label>{{ __('web/product.from') }}</label>		
+                <input type="date" name="" value="2019-12-26">	
             </div>	
             <div>	
-                <label>Do</label>		
-                <input type="date" name="dateTo" value="2019-12-26">	
+                <label>{{ __('web/product.to') }}</label>		
+                <input type="date" name="" value="2019-12-26">	
             </div>
-            <button type="submit" class="product-view__reservation__submit">Złóż ofertę rezerwacji</button>
+            <button type="submit" class="product-view__reservation__submit">{{ __('web/product.reservation_button') }}</button>
         </form>
 
-        <div class="product-view__map__title">Zobacz jak do nas dojechać</div>
+        <div class="product-view__map__title">{{ __('web/product.map') }}</div>
         <div class="product-view__map">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1533.3945083810524!2d16.921737224749837!3d52.40464562802438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x12916d24a02efb00!2sWy%C5%BCsza%20Szko%C5%82a%20Bankowa%20w%20Poznaniu!5e0!3m2!1spl!2spl!4v1579452815952!5m2!1spl!2spl"
